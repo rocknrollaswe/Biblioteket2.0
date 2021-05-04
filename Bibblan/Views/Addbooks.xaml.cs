0bd55973a13071d0f8b627aac2f55bf349456663
Julia Jonsson <70626986+julijonsson@users.noreply.github.com>
2021-05-04 14:51:04 +0200

    add books

---
+++ Bibblan/Views/Addbooks.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bibblan.Views
+{
+    /// <summary>
+    /// Interaction logic for Addbooks.xaml
+    /// </summary>
+    public partial class Addbooks : Window
+    {
+        public Addbooks()
+        {
+            InitializeComponent();
+        }
+    }
+}
