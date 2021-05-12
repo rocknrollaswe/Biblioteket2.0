db507a2fd3b63a97331e497d487d7eae60f465c6
Morgan-Mauritz <morganmauritzhedberg@gmail.com>
2021-05-12 14:46:57 +0200

    login funktion + globala variabler

--- Bibblan/Views/AdminPage.xaml.cs
+++ Bibblan/Views/AdminPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using Bibblan.Services;
 
 namespace Bibblan.Views
 {
@@ -21,6 +22,7 @@ namespace Bibblan.Views
         public AdminPage()
         {
             InitializeComponent();
+            MessageBox.Show(GlobalClass.userPermission.ToString()); //ENDAST FÖR ATT SE SÅ LOGIN FUNGERAR. SKA TAS BORT
         }
         private void createUserClick(object sender, RoutedEventArgs e)
         {
@@ -36,6 +38,8 @@ namespace Bibblan.Views
         {
             NavigationService nav = NavigationService.GetNavigationService(this);
             nav.Navigate(new Login());
+            GlobalClass.userFirstName = null;
+            GlobalClass.userPermission = null;
         }
 
         private void searchClick(object sender, RoutedEventArgs e)
