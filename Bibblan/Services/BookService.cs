c413b3b8726a9d326d33fcde65e047cd0c3d076a
Morgan-Mauritz <morganmauritzhedberg@gmail.com>
2021-06-02 11:23:53 +0200

    ordnade error i addBooks

--- Bibblan/Services/BookService.cs
+++ Bibblan/Services/BookService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Bibblan.Models; 
 
@@ -7,7 +8,7 @@ namespace Bibblan.Services
 {
     public static class BookService
     {
-        public static void AddBook(string title, string author, string description, string edition, string price, string ddk, string sab, string publisher, int isEbook1Else0)
+        public static Book AddBook(string title, string author, string description, string edition, string price, string ddk, string sab, string publisher, int isEbook1Else0)
         {
             var book = new Book();
 
@@ -23,6 +24,8 @@ namespace Bibblan.Services
 
             DbInitialiser.Db.Add(book); 
             DbInitialiser.Db.SaveChanges();
+
+            return book;
         }
         public static Loanlog AddLoanlog(int stockId, int userId, DateTime loanDate, DateTime returnDate)
         {
@@ -37,6 +40,25 @@ namespace Bibblan.Services
             DbInitialiser.Db.SaveChanges();
 
             return loanLog;
+        }
+
+        public static void AddStockBook(Book book, int amount)
+        {
+            IEnumerable<Book> isbnBook = DbInitialiser.Db.Books.Where
+                (b => b.Title == book.Title && b.Edition == book.Edition); // hämtar isbn för den nyss tillagda boken
+
+            Book b = isbnBook.FirstOrDefault();
+
+            for (int i = 0; i < amount; i++)
+            {
+                var stock = new Stock();
+                stock.Isbn = Convert.ToInt32(b.Isbn);
+                stock.Condition = "Nyskick";
+                stock.Discarded = 0;
+                stock.Available = 1;
+                DbInitialiser.Db.Add(stock);
+            }
+            DbInitialiser.Db.SaveChanges();
         }
     }
 }
