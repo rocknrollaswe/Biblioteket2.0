35f97100b24adc627beecad2992b4774222c611f
Morgan-Mauritz <morganmauritzhedberg@gmail.com>
2021-06-03 11:50:30 +0200

    fixade reverteringen

--- Bibblan/Services/UserService.cs
+++ Bibblan/Services/UserService.cs
@@ -1,7 +1,9 @@
 ﻿using Bibblan.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace Bibblan.Services
 {
@@ -24,12 +26,29 @@ namespace Bibblan.Services
 
             return registeredUser;
         }
-        public static void Login(int userPermission, string userFirstName, int userId, byte? loanPermission)
+        public static void Login(string emailBox, string passwordTextBox)
         {
-            GlobalClass.userPermission = userPermission;
-            GlobalClass.userFirstName = userFirstName;
-            GlobalClass.currentUserID = userId;
-            GlobalClass.loanPermission = loanPermission;
+            string emailLow = emailBox.ToLower();
+            var userList = DbInitialiser.Db.Users.ToList();
+            var connectedUser = userList.Find(x => x.Email == emailLow);
+            if (connectedUser != null)
+            {
+                if (connectedUser.Password.SequenceEqual(Encryption.Encrypt(passwordTextBox)) == true) //SequenceEqual går igenom ByteArrayerna och checkar värdena mot varandra. Detta är en långsam funktion, dock så funkar den då vi inte har 10000 användare
+                {
+                    GlobalClass.userPermission = connectedUser.Permissions;  //sätter våra globala variabler för den specifika användaren
+                    GlobalClass.userFirstName = connectedUser.Firstname;       //FYLL PÅ HÄR OM VI BEHÖVER FLER GLOBALA VARIABLER
+                    GlobalClass.currentUserID = connectedUser.UserId;
+                    GlobalClass.loanPermission = connectedUser.HasLoanCard;
+                }
+                else
+                {
+                    MessageBox.Show("Fel uppgifter angivna");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fel uppgifter angivna");
+            }
         }
     }
 }
--- BibblanTest/BibblanTest.cs
+++ BibblanTest/BibblanTest.cs
@@ -21,63 +21,31 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
 namespace BibblanTest
-{ 
+{
     [TestClass]
-      
+
     public class UnitTest1
     {
-
-        [TestMethod]
-        public void AddBookTest()
-        {
-            //arrange
-            string title = "Flugornas Herre";
-            string author = "William Golding";
-            string description = "Efter en flygkrasch upptäcker en grupp engelska skolpojkar(6 - 12 år) att de befinner sig på en öde ö i Stilla havet." +
-                "Pojkarna har evakuerats från England på grund av ett kärnvapenkrig.Ingen vuxen finns på ön och pojkarna organiserar sig så gott de kan, glada över sin nyvunna frihet.";
-            string edition = "1959";
-            string price = "10";
-            string ddk = "800";
-            string sab = "H";
-            string publisher = "Faber and Faber";
-            int isEbook = 0;
-
-            Book actual = new Book() { Title = title, Author = author, Description = description, Edition = int.Parse(edition), Price = int.Parse(price), Ddk = int.Parse(ddk), Sab = sab, Publisher = publisher, Category = isEbook };
-            
-
-            Book bookToCheck = BookService.AddBook(title, author, description, edition, price, ddk, sab, publisher, isEbook);
-
-            //assert
-            Assert.AreEqual(actual, bookToCheck);
-
-            BiblioteketContext.testConnectionString = "Server = tcp:bladerunnerdb.database.windows.net,1433; Initial Catalog = Biblioteket_Kopiera; Persist Security Info = False; User ID = harrison; " +
-                "Password = Blade1234; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30"; 
-
-            DbInitialiser.InitialiseDB(); 
-
-            DbInitialiser.Db.Books.Add(bookToCheck);
-            DbInitialiser.Db.SaveChanges(); 
-
-        }
         [TestClass]
         public class Login
         {
             [TestMethod]
             public void AuthorizedAccessTest() // Test for Admin
             {
-                BookService.Login("email@gmail.com", "Password1");
+                UserService.Login("email@gmail.com", "Password1");
                 Assert.AreEqual(2, GlobalClass.userPermission);
             }
             [TestMethod]
             public void UnauthorizedAccessTest() // Test for Wrong User
             {
                 BiblioteketContext.testConnectionString = "Server = tcp:bladerunnerdb.database.windows.net,1433; Initial Catalog = Biblioteket_Kopiera; Persist Security Info = False; User ID = harrison; " +
-"Password = Blade1234; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30";
-
+                                                          "Password = Blade1234; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30";
                 DbInitialiser.InitialiseDB();
-                GlobalClass.userPermission = -99;
-                BookService.Login("evilhacker@email.com", "Password1");
-                Assert.IsFalse(BookEquals(null, GlobalClass.userPermission));
+
+                UserService.Login("evilhacker@email.com", "Password1");
+                Assert.IsFalse(BookEquals(0, GlobalClass.userPermission));
+                Assert.IsFalse(BookEquals(1, GlobalClass.userPermission));
+                Assert.IsFalse(BookEquals(2, GlobalClass.userPermission));
             }
             bool BookEquals(int? Incorrectvalue, int? GlobalClass)   //Metod som kollar om böckerna är 'likadana', vår tidigare override i Equals i Booksmodellen skapade nya problem med grundläggande funktioner 
             {
@@ -88,12 +56,4 @@ namespace BibblanTest
             }
         }
     }
-
-    
-
-
-
-
-
-
 }
