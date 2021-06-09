73bafa27cff04969cc425abfa316485e9ed30c65
RocknRollaSwe <d.inghamn@gmail.com>
2021-06-09 11:55:15 +0200

    'changesinServices'

---
+++ Bibblan/Services/LogOut.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bibblan.Services; 
+
+namespace Bibblan.Services
+{
+    public static class LogOut
+    {
+        public static void LogOutUser() 
+        {
+            GlobalClass.currentUserID = null;
+            GlobalClass.chosenBook = null;
+            GlobalClass.chosenBookReport = null;
+            GlobalClass.deletedObjects = null;
+            GlobalClass.loanPermission = null;
+            GlobalClass.userFirstName = null;
+            GlobalClass.userPermission = null; 
+        
+        }
+
+    }
+}
